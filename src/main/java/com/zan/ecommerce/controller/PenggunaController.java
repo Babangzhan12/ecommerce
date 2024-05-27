d6cd0493d85879a04d5c98193bc4f13779298e31
Fauzan Ahmad <babangzhan1@gmail.com>
2024-05-27 22:55:23 +0700

    "Create Pengguna MVC"

---
+++ src/main/java/com/zan/ecommerce/controller/PenggunaController.java
@@ -0,0 +1,48 @@
+package com.zan.ecommerce.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zan.ecommerce.entity.Pengguna;
+import com.zan.ecommerce.service.PenggunaService;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/api")
+public class PenggunaController {
+    
+    @Autowired
+    private PenggunaService penggunaService;
+
+    @GetMapping("/penggunas")
+    public List<Pengguna> findall(){
+        return penggunaService.findAll();
+    }
+
+    @GetMapping("/penggunas/{id}")
+    public Pengguna findById(@PathVariable("id") String id){
+        return penggunaService.findById(id);
+    }
+    @PostMapping("/penggunas")
+    public Pengguna create(@RequestBody Pengguna pengguna){
+        return penggunaService.create(pengguna);
+    }
+
+    @PutMapping("/penggunas")
+    public Pengguna edit(@RequestBody Pengguna pengguna) {
+        return penggunaService.edit(pengguna);
+    }
+
+    @DeleteMapping("/penggunas/{id}")
+    public void deleteById(@PathVariable("id") String id){
+        penggunaService.deleteById(id);
+    }
+}
--- src/main/java/com/zan/ecommerce/repository/PenggunaRepository.java
+++ src/main/java/com/zan/ecommerce/repository/PenggunaRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.zan.ecommerce.entity.Pengguna;
 
 public interface PenggunaRepository extends JpaRepository<Pengguna, String>{
+
+    boolean existsByEmail(String email);
     
 }
